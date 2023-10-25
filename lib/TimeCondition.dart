74eb2a9e177baba8dacc182e857bd03231ef54bc
dLogan807 <dylanlogan316@gmail.com>
2023-10-25 20:40:34 +1300

    add time condition

--- lib/TimeCondition.dart
+++ lib/TimeCondition.dart
@@ -5,12 +5,20 @@ class TimeCondition extends Condition {
 
   TimeCondition(super.inverted, super.disabled, this.time);
 
-  //If the
+  //If the time
   @override
   bool isMet() {
     DateTime currentTime = DateTime.now();
 
-    return true;
+    return areSameTime(currentTime);
   }
 
+  //If the passed time is the same as the stored time
+  bool areSameTime(DateTime otherTime) {
+    if (time.hour == otherTime.hour && time.minute == otherTime.minute) {
+      return true;
+    }
+
+    return false;
+  }
 }
